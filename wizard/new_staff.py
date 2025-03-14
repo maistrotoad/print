1f3e5247a3002b3fcb707ed7f86fe159a0ec2e15
Michiel Gielen <maistrotoad@gmail.com>
2025-03-14 17:06:39 +0100

    advancing the staff

--- wizard/new_staff.py
+++ wizard/new_staff.py
@@ -21,8 +21,10 @@ housing_height = 220
 
 starter_height = 50
 
-slope_height = 10
+slope_height = 5
 buldge_diameter = 10
+
+tolerance = 0.1
 
 battery_housing = (
     cq.Workplane("XY")
@@ -53,34 +55,87 @@ battery_housing = (
 ov.show(battery_housing, colors=["grey"], alphas=[1])
 # %%
 
-battery_housing_top = (
+battery_housing = (
     battery_housing.faces(">Z")
+    .polygon(
+        nSides=8,
+        circumscribed=True,
+        diameter=staff_diameter,
+    )
+    .workplane(offset=slope_height * 0.8)
+    .polygon(
+        nSides=8, circumscribed=True, diameter=staff_diameter + buldge_diameter
+    )
+    .loft(ruled=True)
+    .faces(">Z")
+    .polygon(
+        nSides=8,
+        circumscribed=True,
+        diameter=staff_diameter - wall_thickness * 2,
+    )
+    .polygon(
+        nSides=8,
+        circumscribed=True,
+        diameter=staff_diameter + buldge_diameter,
+    )
+    .extrude(slope_height * 0.4)
+    .faces(">Z")
+    .polygon(
+        nSides=8,
+        circumscribed=True,
+        diameter=staff_diameter + buldge_diameter,
+    )
+    .workplane(offset=slope_height * 0.8)
+    .polygon(
+        nSides=8,
+        circumscribed=True,
+        diameter=staff_diameter,
+    )
+    .loft(ruled=True)
+    .faces(">Z")
+    .polygon(
+        nSides=8,
+        circumscribed=True,
+        diameter=staff_diameter - wall_thickness * 2,
+    )
+    .cutBlind(-slope_height * 2)
+    .faces(">Z")
+    .polygon(
+        nSides=8,
+        circumscribed=True,
+        diameter=staff_diameter - wall_thickness * 2,
+    )
     .polygon(nSides=8, circumscribed=True, diameter=staff_diameter)
-    .workplane(offset=slope_height * 0.5)
-    .polygon(
-        nSides=8, circumscribed=True, diameter=staff_diameter + buldge_diameter
-    )
-    .loft()
-    .faces(">Z")
-    .polygon(
-        nSides=8,
-        circumscribed=True,
-        diameter=staff_diameter - wall_thickness * 2,
-    )
-    .cutBlind(-slope_height)
-    .faces(">Z")
-    .polygon(
-        nSides=8,
-        circumscribed=True,
-        diameter=staff_diameter - wall_thickness * 2,
-    )
-    .polygon(
-        nSides=8, circumscribed=True, diameter=staff_diameter + buldge_diameter
-    )
-    .extrude(slope_height * 0.5)
+    .extrude(starter_height - slope_height)
 )
 
-ov.show(battery_housing_top, colors=["grey"], alphas=[1])
+ring_cap = (
+    cq.Workplane("XY")
+    .workplane(offset=starter_height - slope_height - wall_thickness * 0.5)
+    .polygon(
+        nSides=8,
+        circumscribed=True,
+        diameter=staff_diameter + tolerance * 2,
+    )
+    .polygon(
+        nSides=8,
+        circumscribed=True,
+        diameter=staff_diameter + buldge_diameter + wall_thickness * 2,
+    )
+    .extrude(slope_height * 2)
+)
+
+extension = battery_housing.split(keepTop=True)
+battery_housing = battery_housing.split(keepBottom=True)
+
+ov.show(
+    battery_housing,
+    extension,
+    ring_cap,
+    ring_cap.faces("<X"),
+    colors=["grey", "darkblue"],
+    alphas=[1, 1, 0.75],
+)
 
 
 # %%
