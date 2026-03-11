416f08f87fcd543e621668a9a0a34d33b3401eea
Michiel Gielen <maistrotoad@gmail.com>
2026-03-11 11:27:10 +0100

    vincent keychain

--- vincent/main.py
+++ vincent/main.py
@@ -9,25 +9,58 @@ D = 5
 H = 2 * D
 S = H / 10
 TH: int | float = S / 10
-txt_a = "VNET"
 
-txt_b = "ICN"
+# %%
 
-# planar
-render_a = cf.text(txt_a, 1, font="MesloLGS NF", kind="bold")
-render_b = cf.text(txt_b, 1, font="MesloLGS NF", kind="bold")
 
-name_a = cf.offset(render_a, TH * 2).moved(z=-TH * 0.5)
-name_b = cf.offset(render_b, TH)
+render_v = cf.offset(
+    cf.text("V", 1, font="MesloLGS NF", kind="bold"), TH * 2
+).moved(z=-TH * 0.5)
+
+render_i = cf.offset(
+    cf.text("I", 1, font="MesloLGS NF", kind="bold"), TH
+).moved(x=0.31)
+
+render_n = cf.offset(
+    cf.text("N", 1, font="MesloLGS NF", kind="bold"), TH * 2
+).moved(z=-TH * 0.5, x=0.72)
+
+render_c = cf.offset(
+    cf.text("C", 1, font="MesloLGS NF", kind="bold"), TH
+).moved(x=1.08)
+
+render_e = cf.offset(
+    cf.text("E", 1, font="MesloLGS NF", kind="bold"), TH * 2
+).moved(z=-TH * 0.5, x=1.48)
+
+render_n2 = cf.offset(
+    cf.text("N", 1, font="MesloLGS NF", kind="bold"), TH
+).moved(x=1.83)
+
+render_t = cf.offset(
+    cf.text("T", 1, font="MesloLGS NF", kind="bold"), TH * 2
+).moved(z=-TH * 0.5, x=2.202)
+
+vincent = cq.Workplane(
+    cf.compound(
+        render_v, render_i, render_n, render_c, render_e, render_n2, render_t
+    )
+)
 
 ring = (
     cq.Workplane("XY")
     .circle(S * 0.2)
     .circle(S * 0.1)
-    .extrude(TH)
-    .translate((-1.16, 0, 0))
+    .extrude(TH * 2)
+    .translate((-0.255, 0, -TH * 0.5))
 )
 
-ov.show(name_a, name_b, ring)
+vincent = vincent.union(ring)
 
+ov.show(vincent)
+
+# %%
+
+
+vincent.export("vincent.stl")
 # %%
